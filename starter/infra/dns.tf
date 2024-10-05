9b903f3e5f503c8f5ecbd1626e78f7d7cb594812
PD-CLOUDITIN <pradip.k.dalvi@outlook.com>
2024-10-05 08:45:16 +0700

    cofig change

--- starter/infra/dns.tf
+++ starter/infra/dns.tf
@@ -1,19 +1,19 @@
-locals {
-  ingress_hostname = length(kubernetes_service.blue.status[0].load_balancer[0].ingress) > 0 ? kubernetes_service.blue.status[0].load_balancer[0].ingress[0].hostname : "default.example.com"
-}
-resource "aws_route53_record" "blue" {
-  zone_id = aws_route53_zone.private_dns.id
-  name    = "blue-green.udacityproject.com"
-  type    = "CNAME"
-  ttl     = 5
+# locals {
+#   ingress_hostname = length(kubernetes_service.blue.status[0].load_balancer[0].ingress) > 0 ? kubernetes_service.blue.status[0].load_balancer[0].ingress[0].hostname : "default.example.com"
+# }
+# resource "aws_route53_record" "blue" {
+#   zone_id = aws_route53_zone.private_dns.id
+#   name    = "blue-green.udacityproject.com"
+#   type    = "CNAME"
+#   ttl     = 5
 
-  weighted_routing_policy {
-    weight = 2
-  }
+#   weighted_routing_policy {
+#     weight = 2
+#   }
 
-  set_identifier = "blue"
-  records        = [local.ingress_hostname]
-  # https://github.com/hashicorp/terraform-provider-kubernetes/pull/1125
+#   set_identifier = "blue"
+#   records        = [local.ingress_hostname]
+#   # https://github.com/hashicorp/terraform-provider-kubernetes/pull/1125
 }
 
 
