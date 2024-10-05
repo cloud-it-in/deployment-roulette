d3d9a75a9358d0900e0cfa10e1ba268478dd0057
PD-CLOUDITIN <pradip.k.dalvi@outlook.com>
2024-10-05 08:27:00 +0700

    updated the DNS for green

--- starter/infra/dns.tf
+++ starter/infra/dns.tf
@@ -3,7 +3,7 @@ locals {
 }
 resource "aws_route53_record" "blue" {
   zone_id = aws_route53_zone.private_dns.id
-  name    = "blue-green"
+  name    = "blue-green.udacityproject.com"
   type    = "CNAME"
   ttl     = 5
 
@@ -16,6 +16,24 @@ resource "aws_route53_record" "blue" {
   # https://github.com/hashicorp/terraform-provider-kubernetes/pull/1125
 }
 
+
+locals {
+  ingress_hostname = length(kubernetes_service.blue.status[0].load_balancer[0].ingress) > 0 ? kubernetes_service.green.status[0].load_balancer[0].ingress[0].hostname : "default.example.com"
+}
+resource "aws_route53_record" "green" {
+  zone_id = aws_route53_zone.private_dns.id
+  name    = "blue-green.udacityproject.com"
+  type    = "CNAME"
+  ttl     = 5
+
+  weighted_routing_policy {
+    weight = 2
+  }
+
+  set_identifier = "green"
+  records        = [local.ingress_hostname]
+  # https://github.com/hashicorp/terraform-provider-kubernetes/pull/1125
+}
 resource "aws_route53_zone" "private_dns" {
   name    = "udacityproject"
   comment = "DNS for Udacity Projects"
